8484c3ff6bf9e31886ea0c81cdd34ef590a16fe6
ditatara <76758657+ditatara@users.noreply.github.com>
2022-10-17 20:17:33 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:tugas1dosen/pages/home.dart';
+import 'pages/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        builder: (context, widget) => ResponsiveWrapper.builder(
+              ClampingScrollWrapper.builder(context, widget!),
+              breakpoints: [
+                ResponsiveBreakpoint.resize(350, name: MOBILE),
+                ResponsiveBreakpoint.resize(600, name: TABLET),
+                ResponsiveBreakpoint.resize(800, name: DESKTOP),
+                ResponsiveBreakpoint.resize(1700, name: 'XL'),
+              ],
+            ),
+        debugShowCheckedModeBanner: false,
+        title: 'aplikasi opesrasi',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Login());
+  }
+}
